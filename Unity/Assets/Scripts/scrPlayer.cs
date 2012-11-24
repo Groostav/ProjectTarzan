7e59b498854dd6d55ff07a7f5e5bfdb873700864
Geoff Groos <geoff_groos@msn.com>
2012-11-24 09:30:11 -0800

    added a test bar, tweaked the players source

--- Unity/Assets/Scripts/scrPlayer.cs
+++ Unity/Assets/Scripts/scrPlayer.cs
@@ -1,110 +1,76 @@
 using UnityEngine;
 using System.Collections;
 
-public class scrPlayer : MonoBehaviour 
-{		
-	public Texture2D textureWin;	// Victory Image
-	public const float buffSpace = 10.0f; // space around GUI stuff, so it's not just against the edge 
-	private const int textureWinWidth = 128;	// texture width of win message
-	private const int textureWinHeight = 128;	// texture height of win message
-	private bool isVictory = false;		// victory condition
+public class scrPlayer : MonoBehaviour
+{
+    private float strafeSpd = 0.6f; //horizontal strafe speed
+    private float fbSpd = 0.6f; //forward backwards speed	
+    private float rotSpd = 2.0f; //rotation speed per frame
+    private float jumpForce = 2000.0f; // jump force
 
-	private float strafeSpd = 0.6f;	//horizontal strafe speed
-	private float fbSpd = 0.6f;	//forward backwards speed	
-	private float rotSpd = 2.0f; //rotation speed per frame
-	private float jumpForce = 500.0f; // jump force
+    public bool isTouchedGround = false; //Has the player touched the ground since his last jump?
 
-    public bool isTouchedGround = false;	//Has the player touched the ground since his last jump?
+    private GameObject ropeShotSpot; // A handel to the rope's shot spot
 
 
-    private float horizontalSpeed = 0.2f;//strafe speed
-    private float verticalSpeed = 0.2f;//forward back speed
-    private float rotateSpeed = 2.0f;//rotation speed
-	
-	
-	
-	
-	// Use this for initialization
-	void Start () 
-	{	
-		//nothing
-	}
-	
-	
-	
-	
-	// Update is called once per frame
-	void Update () 
-	{
-		//nothing
-	}
-	
-	
-	
-	
-	//guarnteed to run each frame
-	void FixedUpdate()
-	{
-				//player directional movement
-		float s = strafeSpd * Input.GetAxis("Strafe");
-		float fb = fbSpd * Input.GetAxis("ForwardBack");
-		transform.Translate(s, 0, fb);	//movement x, and z
+    // Use this for initialization
+    private void Start()
+    {
+        //nothing
+        //Physics.gravity = Vector3.zero;
+    }
 
-        float h = horizontalSpeed * Input.GetAxis("Horizontal");
-        float v = verticalSpeed * Input.GetAxis("Vertical");
 
-        var player = GameObject.Find("Player");
 
-//        if (Input.GetButtonDown("Jump")/* && isTouchedGround*/)//jump up on player's Y
-//        {
-//            var jumpingForce = new Vector3(jumpForce * (h * 10), jumpForce, jumpForce * (v * 10));//push the player 		
-//            rigidbody.AddForce(jumpingForce);
-//            isTouchedGround = false;
-//            //fallForce = player.rigidbody.velocity;
-//            print(jumpingForce);
-//        }
 
-			// Player horizontal camera movement
-		if(Input.GetButton("Fire2"))
-		{
-			float rotY = rotSpd * Input.GetAxis("MouseLookX");
-			transform.Rotate(Vector3.up, rotY);	//rotate player around y
-		}
-		
-	}
-	
-		
-	
-	
-		//Check for player collision with the ground or goal
-	void OnCollisionEnter(Collision hit)
-	{
-			//if player has hit the ground, they can jump again.
-	    isTouchedGround = true;
-		
-			//if the player has reached the goal
-		if(hit.gameObject.name == "goal")
-		{
-			isVictory = true;		
-		}
-	}
-	
-	
-	
-	
-		//Graphical User Interface (For Victory screen)
-	void OnGUI()
-	{
-		if(isVictory)
-		{
-			print ("You won!!!!!");
-			
-				//image box for when you win
-			GUI.DrawTexture(new Rect(Screen.width/2.0f - textureWinWidth/2.0f, Screen.height/2.0f - textureWinHeight/2.0f, textureWinWidth, textureWinHeight), textureWin);	
-				//Text box for when you win
-			GUI.Box(new Rect(Screen.width/2.0f - textureWinWidth/2.0f, Screen.height/2.0f - textureWinHeight/2.0f, textureWinWidth, textureWinHeight), "You Won!!!");	
-		}
-	}
+    // Update is called once per frame
+    private void Update()
+    {
+        //nothing
+    }
+
+
+
+
+    //guarnteed to run each frame
+    private void FixedUpdate()
+    {
+
+        //		rigidbody.AddForce(0.0f, gravityForce, 0.0f);//apply gravity down
+
+        //player directional movement
+        float s = strafeSpd*Input.GetAxis("Strafe");
+        float fb = fbSpd*Input.GetAxis("ForwardBack");
+        transform.Translate(s, 0, fb); //movement x, and z
+
+
+        //Player jump ability (has to have touched the ground since last jump)
+        if (Input.GetButtonDown("Jump") && isTouchedGround )
+        {
+            //Allow the player to jump off of their rope
+            this.gameObject.GetComponentInChildren<scrShootRope>().isSwinging = false;
+
+            rigidbody.AddForce(0, jumpForce, 0); //push player up
+            isTouchedGround = false; //They are no longer touching the ground
+        }
+        // Player horizontal camera movement
+        if (Input.GetButton("Fire2"))
+        {
+            float rotY = rotSpd*Input.GetAxis("MouseLookX");
+            transform.Rotate(Vector3.up, rotY); //rotate player around y
+        }
+
+    }
+
+
+
+
+    //Check for player collision with the ground or goal
+    private void OnCollisionEnter(Collision hit)
+    {
+        //if player has hit the ground, they can jump again.
+        isTouchedGround = hit.gameObject.name == "l_canyon";
+
+        return;
+    }
 }
-
-
